8489049f69951cf86882a2868a6a0c0cec2ffff2
Óliver Hierro <ohierro@gmail.com>
2022-06-03 12:41:19 +0200

    Fix some error. Added router to dependencies. Changed view names to prevent linter problems

--- front_good/marvieritos/src/App.vue
+++ front_good/marvieritos/src/App.vue
@@ -1,29 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
   <div id="nav">
-    <router-link to="/home">Home</router-link> |
-      <router-link to="/products">Products</router-link> |
-      <router-link to="/cart">Cart</router-link>
-
-    </div>
-    <h1 v-if="isMock">Working with MOCK data</h1>
-    <router-view/>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-
-export default {
-  created(){
-    fetch(("https://localhost:44330/api/Products"))
-  },
-  computed: {
-    isMock() {
-      return process.env.VUE_APP_API_IS_MOCK === "true"
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -32,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
